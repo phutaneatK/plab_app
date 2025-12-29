b5240b266eddfc85f4530c8dd9bc8898272d2b68
phutaneat <phutaneat.k@cubesofttech.com>
2025-12-29 14:38:14 +0700

    feat : update icon login page

--- lib/presentation/login/pages/login_page.dart
+++ lib/presentation/login/pages/login_page.dart
@@ -84,7 +84,7 @@ class _LoginPageState extends State<LoginPage> {
                       children: [
                         // Logo
                         Icon(
-                          Icons.rocket_launch,
+                          Icons.star_border,
                           size: 80,
                           color: Colors.blue.shade700,
                         ),
